dc01b00fcc107f1aa6e813cc730dfa5e73ad9de8
Ryan Baumann <ryan.baumann@gmail.com>
2012-05-28 12:01:02 -0400

    Format to html5, add some front matter

--- yaml2html.rb
+++ yaml2html.rb
@@ -7,7 +7,7 @@ require 'yaml'
 associations = YAML::load STDIN
 
 template = IO.read(File.join('index.haml'))
-haml_engine = Haml::Engine.new(template)
+haml_engine = Haml::Engine.new(template, :format => :html5)
 open('index.html','w') {|file|
   file.write(haml_engine.render(Object.new, :associations => associations))
 }

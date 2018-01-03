2d0d6c36ae6d765e6c2253ea752cba1d7fbab032
Ryan Baumann <ryan.baumann@gmail.com>
2018-01-03 11:27:47 -0500

    Update yaml2rdf/yaml2html gem deps

--- yaml2html.rb
+++ yaml2html.rb
@@ -3,11 +3,12 @@
 require 'rubygems'
 require 'haml'
 require 'yaml'
+require 'htmlbeautifier'
 
 associations = YAML::load STDIN
 
 template = IO.read(File.join('index.haml'))
 haml_engine = Haml::Engine.new(template, :format => :html5)
 open('index.html','w') {|file|
-  file.write(haml_engine.render(Object.new, :associations => associations))
+  file.write(HtmlBeautifier.beautify(haml_engine.render(Object.new, :associations => associations)))
 }
